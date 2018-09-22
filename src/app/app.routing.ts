f0c3b8d2e582b6d52349ff3d0dcf4f34834ba4f5
dhinta <relaxed.dhinta@gmail.com>
2018-09-22 15:42:47 +0530

    Html/Css updated

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,25 +1,33 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-import { PublicBaseComponent } from './components/public-base/public-base.component';
-import { PrivateBaseComponent } from './components/private-base/private-base.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { PublicBaseComponent } from "./components/public-base/public-base.component";
+import { PrivateBaseComponent } from "./components/private-base/private-base.component";
+import { HomeComponent } from "./components/pages/home/home.component";
+import { DashboardComponent } from "./components/pages/dashboard/dashboard.component";
 
-export const appRoutes: Routes = [{
+export const appRoutes: Routes = [
+  {
     path: "",
     component: PublicBaseComponent,
-    children: [{
+    children: [
+      {
         path: "",
         component: HomeComponent
-    }]
-}, {
+      }
+    ]
+  },
+  {
     path: "project",
     component: PrivateBaseComponent,
-    children: [{
+    children: [
+      {
         path: "dashboard",
         component: DashboardComponent
-    }]
-}, {
+      }
+    ]
+  },
+  {
     path: "user",
     component: PrivateBaseComponent
-}];
+  }
+];
